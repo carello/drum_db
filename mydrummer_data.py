acb31f415c5b2f80fd75ff04033c491364ef2be9
cpuskarz <cpuskarz@cisco.com>
2016-07-20 21:38:13 -0500

    updated app

--- mydrummer_data.py
+++ mydrummer_data.py
@@ -1,13 +1,14 @@
 from flask import Flask, make_response, request, jsonify, Response
-#import datetime, json, sys, os
-#from collections import Counter
+import datetime, json, sys, os
+from collections import Counter
 
 app = Flask(__name__)
 data_dir = "./"
 data_file = "drummer.txt"
 
-@app.route("/")
-def hero_list():
+
+@app.route("/drummer_list")
+def drummer_list():
     drummer_list = []
     with open("drummer.txt") as f:
         for line in f:
@@ -16,5 +17,34 @@ def hero_list():
     resp = make_response(jsonify(drummers=drummer_list))
     return resp
 
+    
+@app.route("/options", methods=["GET", "PUT", "POST"])
+def options_route():
+    '''
+    Methods used to view options, add new option, and replace options.
+    '''
+    if request.method == "GET":
+        options = {"options":option_list()}
+        status = 200
+    
+    resp = Response(
+        json.dumps(options, sort_keys=True, indent = 4, separators = (',', ': ')),
+        content_type='application/json',
+        status=status)
+    return resp
+
+
+def option_list():
+    '''
+    Get a list of possible options.
+    '''
+    options = []
+    with open(data_file) as f:
+        for line in f:
+            line = line.rstrip()
+            options.append(line)
+    return options
+
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0', port=int("5003"))
